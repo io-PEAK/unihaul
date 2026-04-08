d5c11206aecf706b394afc521d91f5994f88c77e
Peak <159932972+io-PEAK@users.noreply.github.com>
2026-04-08 17:38:52 +0530

    fix render not detecting database url

--- backend/database/prisma.config.ts
+++ backend/database/prisma.config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config'
 import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: 'database/prisma/schema.prisma',
   datasource: {
     url: process.env.DATABASE_URL!,
   },
   migrations: {
-    seed: 'ts-node prisma/seed.ts',
+    seed: 'ts-node database/prisma/seed.ts',
   },
 })
